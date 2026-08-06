7339e4b7b91051901431a45f7bceedb1513a775a
crllect <crllect@proton.me>
2026-08-06 13:11:09 -0400

    Removed examples from git

--- examples/react/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import ProxyShell from "./ProxyShell.tsx";
-
-const root = document.getElementById("root")!;
-
-createRoot(root).render(<ProxyShell />);
